222e60606237da43d0b8a32c3482fb927b2659f8
khushi6420 <khushi090320@gmail.com>
2026-04-12 10:34:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var btn = document.querySelector('button')
+var percent = document.querySelector('#percent')
+var growth = document.querySelector('#growth')
+var grow= 0
+
+btn.addEventListener('click',function(){
+    var int = setInterval(function(){
+        grow++
+        percent.innerHTML = grow + '%'
+        growth.style.width = grow + '%'
+    },50)
+
+    setTimeout(() => {
+        clearInterval(int)
+        btn.innerHTML = 'Downloaded'
+        btn.style.opacity = 0.5
+    },5000)
+})
